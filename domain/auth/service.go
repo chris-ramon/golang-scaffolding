c19f6fc2d6f7ca4dd80d5cd3bafb2f35fbbbb301
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-16 16:49:13 -0500

    domain: wires service CurrentUser

--- domain/auth/service.go
+++ domain/auth/service.go
@@ -1,6 +1,16 @@
 package auth
 
+import (
+	"github.com/chris-ramon/golang-scaffolding/domain/auth/types"
+)
+
 type service struct {
+}
+
+func (s *service) CurrentUser() (types.CurrentUser, error) {
+	return types.CurrentUser{
+		Username: "test",
+	}, nil
 }
 
 func NewService() *service {
