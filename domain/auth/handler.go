60026f8900808afd900987199fb39fa8cac2090e
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-16 16:49:40 -0500

    domain: wires service to handler

--- domain/auth/handler.go
+++ domain/auth/handler.go
@@ -1,12 +1,20 @@
 package auth
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+
+	"github.com/chris-ramon/golang-scaffolding/domain/auth/types"
 )
 
+type Service interface {
+	CurrentUser() (types.CurrentUser, error)
+}
+
 type handlers struct {
+	service Service
 }
 
 func (h *handlers) GetPing() httprouter.Handle {
@@ -17,10 +25,16 @@ func (h *handlers) GetPing() httprouter.Handle {
 
 func (h *handlers) GetCurrentUser() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.Write([]byte("ok"))
+		u, err := h.service.CurrentUser()
+		if err != nil {
+			log.Printf("failed to find current user: %v", err)
+			http.Error(w, "failed to find current user", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(u.Username))
 	}
 }
 
-func NewHandlers() *handlers {
-	return &handlers{}
+func NewHandlers(service Service) *handlers {
+	return &handlers{service: service}
 }
