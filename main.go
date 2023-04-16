34b6a396bdff22c8479b3e2d35064493d4cd1b79
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-16 16:13:19 -0500

    main: wires julienschmidt/httprouter go lib

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 
 	"github.com/graphql-go/handler"
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/chris-ramon/golang-scaffolding/config"
+	"github.com/chris-ramon/golang-scaffolding/domain/auth"
 	"github.com/chris-ramon/golang-scaffolding/gql/schema"
 )
 
@@ -18,13 +20,26 @@ func main() {
 		log.Fatal(err)
 	}
 
+	router := httprouter.New()
+
+	authRoutes := auth.NewRoutes()
+	for _, r := range authRoutes.All() {
+		router.Handle(r.HTTPMethod, r.Path, r.Handler)
+	}
+
 	h := handler.New(&handler.Config{
 		Schema:     &_schema,
 		Pretty:     true,
 		Playground: true,
 	})
 
-	http.Handle("/graphql", h)
+	router.Handle("GET", "/graphql", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		h.ServeHTTP(w, r)
+	})
+	router.Handle("POST", "/graphql", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		h.ServeHTTP(w, r)
+	})
+
 	log.Printf("server running on port :%d", conf.Port)
-	log.Println(http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), nil))
+	log.Println(http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), router))
 }
