55e29ce36464d75afb84f85ae6da3fab9afe573a
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-17 08:22:08 -0500

    domain: adds CurrentUserFromTypeToAPI mapper

---
+++ domain/auth/mappers/mappers.go
@@ -0,0 +1,14 @@
+package mappers
+
+import (
+	"github.com/chris-ramon/golang-scaffolding/domain/auth/api"
+	"github.com/chris-ramon/golang-scaffolding/domain/auth/types"
+)
+
+func CurrentUserFromTypeToAPI(currentUser types.CurrentUser) api.CurrentUser {
+	return api.CurrentUser{
+		ID:       currentUser.ID,
+		Username: currentUser.Username,
+		JWT:      currentUser.JWT,
+	}
+}
