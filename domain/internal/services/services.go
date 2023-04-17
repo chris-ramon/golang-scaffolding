9bbb7bf864e061ad77093419c4fbae18cef60ffa
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-17 12:17:03 -0500

    domain: adds AuthUser method

--- domain/internal/services/services.go
+++ domain/internal/services/services.go
@@ -1,11 +1,13 @@
 package services
 
 import (
+	"context"
 	authTypes "github.com/chris-ramon/golang-scaffolding/domain/auth/types"
 )
 
 type AuthService interface {
 	CurrentUser() (authTypes.CurrentUser, error)
+	AuthUser(ctx context.Context, username string, pwd string) (*authTypes.CurrentUser, error)
 }
 
 type Services struct {
