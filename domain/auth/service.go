a44e0077327f8950ae115b1ca5025e525acfff78
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-17 08:23:07 -0500

    domain: adds initial iteration of AuthUser method

--- domain/auth/service.go
+++ domain/auth/service.go
@@ -1,15 +1,50 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+
 	"github.com/chris-ramon/golang-scaffolding/domain/auth/types"
 )
 
 type service struct {
 }
 
+type customClaims struct {
+	Data map[string]string `json:"data"`
+	jwt.RegisteredClaims
+}
+
 func (s *service) CurrentUser() (types.CurrentUser, error) {
 	return types.CurrentUser{
 		Username: "test user",
+	}, nil
+}
+
+func (s *service) AuthUser(ctx context.Context, username string, pwd string) (*types.CurrentUser, error) {
+	expiresAt := jwt.NewNumericDate(time.Now().Add(3 * time.Minute))
+	data := map[string]string{}
+
+	claims := customClaims{
+		data,
+		jwt.RegisteredClaims{
+			ExpiresAt: expiresAt,
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("local-signing-secret"))
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("token: %v\n", token)
+
+	return &types.CurrentUser{
+		Username: "test user",
+		JWT:      token,
 	}, nil
 }
 
