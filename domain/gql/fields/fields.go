0886452d18ecc2e71588c81f1b36792ac0441bf4
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-17 07:32:40 -0500

    domain: wires auth service

--- domain/gql/fields/fields.go
+++ domain/gql/fields/fields.go
@@ -2,6 +2,8 @@ package fields
 
 import (
 	"github.com/graphql-go/graphql"
+
+	"github.com/chris-ramon/golang-scaffolding/domain/internal/services"
 )
 
 var PingField = &graphql.Field{
@@ -14,6 +16,17 @@ var PingField = &graphql.Field{
 var CurrentUserField = &graphql.Field{
 	Type: graphql.String,
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		return "user", nil
+		rootValue := p.Info.RootValue.(map[string]interface{})
+		srvs, ok := rootValue["services"].(*services.Services)
+		if !ok {
+			return nil, nil
+		}
+
+		currentUser, err := srvs.AuthService.CurrentUser()
+		if err != nil {
+			return nil, err
+		}
+
+		return currentUser.Username, nil
 	},
 }
