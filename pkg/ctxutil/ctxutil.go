ef153d08f59ee36652df5f8e6e9f65d458c9160b
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-20 14:54:56 -0500

    pkg: adds ctxutil.AuthHeaderValueFromCtx

--- pkg/ctxutil/ctxutil.go
+++ pkg/ctxutil/ctxutil.go
@@ -2,9 +2,21 @@ package ctxutil
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"strings"
 )
 
+const authHeaderName = "Authorization"
+
 func WithAuthHeader(ctx context.Context, header http.Header) context.Context {
-	return context.WithValue(ctx, "Authorization", header.Get("Authorization"))
+	return context.WithValue(ctx, authHeaderName, header.Get(authHeaderName))
+}
+
+func AuthHeaderValueFromCtx(ctx context.Context) (string, error) {
+	authorizationWithBearer := strings.Split(ctx.Value(authHeaderName).(string), " ")
+	if len(authorizationWithBearer) != 2 {
+		return "", errors.New("invalid authorization header value")
+	}
+	return authorizationWithBearer[1], nil
 }
