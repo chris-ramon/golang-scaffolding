f1439ddd6c6cdc80649930edc73e875eafe8e949
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-24 15:20:53 -0500

    config: adds DBConfig

--- config/config.go
+++ config/config.go
@@ -1,11 +1,31 @@
 package config
 
+import "os"
+
 type Config struct {
 	Port uint
+}
+
+type DBConfig struct {
+	User    string
+	PWD     string
+	Host    string
+	Name    string
+	SSLMode string
 }
 
 func New(port uint) *Config {
 	return &Config{
 		Port: port,
+	}
+}
+
+func NewDBConfig(user, pwd, host, name, sslMode string) *DBConfig {
+	return &DBConfig{
+		User:    os.Getenv("DB_USER"),
+		PWD:     os.Getenv("DB_PWD"),
+		Host:    os.Getenv("DB_HOST"),
+		Name:    os.Getenv("DB_NAME"),
+		SSLMode: os.Getenv("DB_SSL_MODE"),
 	}
 }
