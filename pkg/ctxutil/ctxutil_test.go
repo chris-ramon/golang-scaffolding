f9e9cb4b138b3d05c604297c663bc16848fc9cb0
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-25 23:29:29 -0500

    pkg: adds AuthHeaderValueFromCtx unit tests

--- pkg/ctxutil/ctxutil_test.go
+++ pkg/ctxutil/ctxutil_test.go
@@ -23,3 +23,33 @@ func TestWithAuthHeader(t *testing.T) {
 		t.Fatalf("expected: %v, got: %v", testAuthHeaderValue, actualCtxValue)
 	}
 }
+
+func TestAuthHeaderValueFromCtx(t *testing.T) {
+	testAuthHeaderValue := "Test-JWT-Token"
+	testAuthHeaderValueWithBearer := "Bearer " + testAuthHeaderValue
+
+	ctx := context.WithValue(context.Background(), AuthHeaderName, testAuthHeaderValueWithBearer)
+
+	actualCtxValue, err := AuthHeaderValueFromCtx(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if actualCtxValue != testAuthHeaderValue {
+		t.Fatalf("expected: %v, got: %v", testAuthHeaderValue, actualCtxValue)
+	}
+}
+
+func TestAuthHeaderValueFromCtx_Error(t *testing.T) {
+	testAuthHeaderValue := "Test-JWT-Token"
+
+	ctx := context.WithValue(context.Background(), AuthHeaderName, testAuthHeaderValue)
+
+	actualCtxValue, err := AuthHeaderValueFromCtx(ctx)
+	if err == nil {
+		t.Fatalf("expected error got nil")
+	}
+	if actualCtxValue != "" {
+		t.Fatalf("expected empty string value got: %v", actualCtxValue)
+	}
+}
