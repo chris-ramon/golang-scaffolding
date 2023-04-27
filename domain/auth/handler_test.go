e7ec040c45dfb029561af10ec145fa11adbef339
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-27 13:30:14 -0500

    domain/auth: adds PostSignIn unit tests

--- domain/auth/handler_test.go
+++ domain/auth/handler_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"io"
@@ -130,5 +131,67 @@ func TestGetCurrentUser_CurrentUserError(t *testing.T) {
 	expectedStatusCode := http.StatusInternalServerError
 	if w.Code != expectedStatusCode {
 		t.Fatalf("expected: %v, got: %v", expectedStatusCode, w.Code)
+	}
+}
+
+func TestPostSignIn(t *testing.T) {
+	srvMock := &serviceMock{
+		authUser: func(ctx context.Context, username string, pwd string) (*types.CurrentUser, error) {
+			return &types.CurrentUser{
+				Username: "test user",
+			}, nil
+		},
+	}
+
+	h := &handlers{
+		service: srvMock,
+	}
+
+	req := httptest.NewRequest(
+		"POST",
+		"/auth/sign-in",
+		bytes.NewBuffer([]byte(`{"email":"test@test.com","password":"test-pwd"}`)),
+	)
+	req.Header.Set("Authorization", "Bearer Test-JWT-Token")
+	w := httptest.NewRecorder()
+	params := httprouter.Params{}
+
+	h.PostSignIn()(w, req, params)
+
+	body, err := io.ReadAll(w.Result().Body)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if string(body) != "test user" {
+		t.Fatalf("expected: ok, got: %s", body)
+	}
+}
+
+func TestPostSignIn_BodyError(t *testing.T) {
+	srvMock := &serviceMock{
+		authUser: func(ctx context.Context, username string, pwd string) (*types.CurrentUser, error) {
+			return &types.CurrentUser{
+				Username: "test user",
+			}, nil
+		},
+	}
+
+	h := &handlers{
+		service: srvMock,
+	}
+
+	req := httptest.NewRequest(
+		"POST",
+		"/auth/sign-in",
+		bytes.NewBuffer([]byte(`{invalid}`)),
+	)
+	w := httptest.NewRecorder()
+	params := httprouter.Params{}
+
+	h.PostSignIn()(w, req, params)
+
+	expectedBody := "failed to json unmarshal request body"
+	if !strings.Contains(w.Body.String(), expectedBody) {
+		t.Fatalf("expected: %v, got: %v", expectedBody, w.Body.String())
 	}
 }
