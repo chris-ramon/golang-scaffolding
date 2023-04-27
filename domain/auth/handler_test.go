1bc4c25db1c269f9ae42585f94754d94ddee5a39
Chris Ramón <rchristian.ramon@gmail.com>
2023-04-27 12:33:33 -0500

    domain/auth: adds GetCurrentUser unit tests

--- domain/auth/handler_test.go
+++ domain/auth/handler_test.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"io"
+	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/chris-ramon/golang-scaffolding/domain/auth/types"
@@ -11,14 +14,16 @@ import (
 )
 
 type serviceMock struct {
+	currentUser func(jwtToken string) (*types.CurrentUser, error)
+	authUser    func(ctx context.Context, username string, pwd string) (*types.CurrentUser, error)
 }
 
 func (s *serviceMock) CurrentUser(jwtToken string) (*types.CurrentUser, error) {
-	return nil, nil
+	return s.currentUser(jwtToken)
 }
 
 func (s *serviceMock) AuthUser(ctx context.Context, username string, pwd string) (*types.CurrentUser, error) {
-	return nil, nil
+	return s.authUser(ctx, username, pwd)
 }
 
 func TestGetPing(t *testing.T) {
@@ -40,5 +45,90 @@ func TestGetPing(t *testing.T) {
 	}
 	if string(body) != "ok" {
 		t.Fatalf("expected: ok, got: %v", body)
+	}
+}
+
+func TestGetCurrentUser(t *testing.T) {
+	srvMock := &serviceMock{
+		currentUser: func(jwtToken string) (*types.CurrentUser, error) {
+			return &types.CurrentUser{
+				Username: "test user",
+			}, nil
+		},
+		authUser: func(ctx context.Context, username string, pwd string) (*types.CurrentUser, error) {
+			return nil, nil
+		},
+	}
+
+	h := &handlers{
+		service: srvMock,
+	}
+
+	req := httptest.NewRequest("GET", "/auth/current-user", nil)
+	req.Header.Set("Authorization", "Bearer Test-JWT-Token")
+	w := httptest.NewRecorder()
+	params := httprouter.Params{}
+
+	h.GetCurrentUser()(w, req, params)
+
+	body, err := io.ReadAll(w.Result().Body)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if string(body) != "test user" {
+		t.Fatalf("expected: ok, got: %s", body)
+	}
+}
+
+func TestGetCurrentUser_AuthHeaderError(t *testing.T) {
+	srvMock := &serviceMock{}
+
+	h := &handlers{
+		service: srvMock,
+	}
+
+	req := httptest.NewRequest("GET", "/auth/current-user", nil)
+	w := httptest.NewRecorder()
+	params := httprouter.Params{}
+
+	h.GetCurrentUser()(w, req, params)
+
+	expectedBody := "failed to get authorization header"
+	if !strings.Contains(w.Body.String(), expectedBody) {
+		t.Fatalf("expected: %v, got: %v", expectedBody, w.Body.String())
+	}
+
+	expectedStatusCode := http.StatusInternalServerError
+	if w.Code != expectedStatusCode {
+		t.Fatalf("expected: %v, got: %v", expectedStatusCode, w.Code)
+	}
+}
+
+func TestGetCurrentUser_CurrentUserError(t *testing.T) {
+	srvMock := &serviceMock{
+		currentUser: func(jwtToken string) (*types.CurrentUser, error) {
+			return nil, errors.New("test error")
+		},
+	}
+
+	h := &handlers{
+		service: srvMock,
+	}
+
+	req := httptest.NewRequest("GET", "/auth/current-user", nil)
+	req.Header.Set("Authorization", "Bearer Test-JWT-Token")
+	w := httptest.NewRecorder()
+	params := httprouter.Params{}
+
+	h.GetCurrentUser()(w, req, params)
+
+	expectedBody := "failed to find current user"
+	if !strings.Contains(w.Body.String(), expectedBody) {
+		t.Fatalf("expected: %v, got: %v", expectedBody, w.Body.String())
+	}
+
+	expectedStatusCode := http.StatusInternalServerError
+	if w.Code != expectedStatusCode {
+		t.Fatalf("expected: %v, got: %v", expectedStatusCode, w.Code)
 	}
 }
