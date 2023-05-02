1d6175bdc52cce3f858a46d701f520d049823664
Chris Ramón <rchristian.ramon@gmail.com>
2023-05-02 17:25:14 -0500

    domain/auth: adds service unit tests

--- domain/auth/service_test.go
+++ domain/auth/service_test.go
@@ -2,51 +2,96 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"testing"
 )
 
 type jwtMock struct {
+	generate func(ctx context.Context, data map[string]string) (*string, error)
+	validate func(ctx context.Context, jwtToken string) (map[string]string, error)
 }
 
 func (j *jwtMock) Generate(ctx context.Context, data map[string]string) (*string, error) {
-	jwtToken := ""
-	return &jwtToken, nil
+	return j.generate(ctx, data)
 }
 
 func (j *jwtMock) Validate(ctx context.Context, jwtToken string) (map[string]string, error) {
-	return map[string]string{}, nil
+	return j.validate(ctx, jwtToken)
 }
 
 func TestCurrentUser(t *testing.T) {
 	type testCase struct {
-		name string
+		name                     string
+		generate                 func(ctx context.Context, data map[string]string) (*string, error)
+		validate                 func(ctx context.Context, jwtToken string) (map[string]string, error)
+		currentUserExpectedError error
+		authUserExpectedError    error
 	}
+
+	testError := errors.New("test error")
 
 	testCases := []testCase{
 		testCase{
 			name: "success",
+			generate: func(ctx context.Context, data map[string]string) (*string, error) {
+				jwtToken := ""
+				return &jwtToken, nil
+			},
+			validate: func(ctx context.Context, jwtToken string) (map[string]string, error) {
+				return nil, nil
+			},
+		},
+		testCase{
+			name: "validate error",
+			generate: func(ctx context.Context, data map[string]string) (*string, error) {
+				jwtToken := ""
+				return &jwtToken, nil
+			},
+			validate: func(ctx context.Context, jwtToken string) (map[string]string, error) {
+				return nil, testError
+			},
+			currentUserExpectedError: testError,
+		},
+		testCase{
+			name: "auth user error",
+			generate: func(ctx context.Context, data map[string]string) (*string, error) {
+				return nil, testError
+			},
+			validate: func(ctx context.Context, jwtToken string) (map[string]string, error) {
+				return nil, nil
+			},
+			authUserExpectedError: testError,
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			srv, err := NewService(&jwtMock{})
+			srv, err := NewService(&jwtMock{
+				generate: testCase.generate,
+				validate: testCase.validate,
+			})
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
 
 			authedUser, err := srv.AuthUser(context.Background(), "user", "pwd")
-			if err != nil {
-				t.Fatalf("unexpected error: %v", err)
+			if testCase.authUserExpectedError != nil {
+				if err != testCase.authUserExpectedError {
+					t.Fatalf("unexpected error: %v", err)
+				}
 			}
 
-			currentUser, err := srv.CurrentUser(authedUser.JWT)
-			if err != nil {
-				t.Fatalf("unexpected error: %v", err)
-			}
-
-			if currentUser == nil {
-				t.Fatalf("unexpected nil value")
+			if authedUser != nil {
+				currentUser, err := srv.CurrentUser(authedUser.JWT)
+				if testCase.currentUserExpectedError != nil {
+					if err != testCase.currentUserExpectedError {
+						t.Fatalf("unexpected error: %v", err)
+					}
+				} else {
+					if currentUser == nil {
+						t.Fatalf("unexpected nil value")
+					}
+				}
 			}
 		})
 	}
