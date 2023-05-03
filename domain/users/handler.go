37bd2135b3331da92d1336b2b2baa4b2a4e555a8
Chris Ramón <rchristian.ramon@gmail.com>
2023-05-03 18:55:53 -0500

    domain/users: wires FinUsers method

--- domain/users/handler.go
+++ domain/users/handler.go
@@ -2,23 +2,42 @@ package users
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
-	userTypes "github.com/chris-ramon/golang-scaffolding/domain/users/types"
 	"github.com/julienschmidt/httprouter"
+
+	"github.com/chris-ramon/golang-scaffolding/domain/users/mappers"
+	userTypes "github.com/chris-ramon/golang-scaffolding/domain/users/types"
 )
 
 type handlers struct {
+	srv Service
 }
 
 func (h *handlers) GetUsers() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.Write([]byte("ok"))
+		users, err := h.srv.FindUsers(r.Context())
+
+		if err != nil {
+			http.Error(w, "failed to find users", http.StatusInternalServerError)
+			return
+		}
+
+		usersAPI := mappers.UsersFromTypeToAPI(users)
+
+		resp, err := json.Marshal(usersAPI)
+		if err != nil {
+			http.Error(w, "failed to marshal response", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(resp)
 	}
 }
 
 func NewHandlers(usersService Service) (*handlers, error) {
-	return &handlers{}, nil
+	return &handlers{srv: usersService}, nil
 }
 
 type Service interface {
