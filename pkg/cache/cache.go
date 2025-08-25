1884ace8cc9b151cd7eb27f3f64d585a72e98e00
Chris Ramón <rchristian.ramon@gmail.com>
2025-08-25 18:35:47 -0500

    pkg/cache: consolidates ttl support

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -7,7 +7,10 @@ import (
 	"github.com/jellydator/ttlcache/v3"
 )
 
-var defaultTTL = 10 * time.Minute
+type TTL time.Duration
+
+var DefaultTTL = 2 * time.Minute
+var NoTTL *TTL = nil
 
 // Cache represents a cache layer.
 type Cache[K comparable, V any] struct {
@@ -19,27 +22,44 @@ type Cache[K comparable, V any] struct {
 }
 
 // Set sets given key value data to the cache.
-func (c *Cache[K, V]) Set(key K, value V) {
+func (c *Cache[K, V]) Set(key K, value V, ttl *TTL) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.cache.Set(key, value, defaultTTL)
+	itemTTL := DefaultTTL
+
+	if ttl != nil {
+		itemTTL = time.Duration(*ttl)
+	}
+
+	if ttl == NoTTL {
+		itemTTL = ttlcache.NoTTL
+	}
+
+	c.cache.Set(key, value, itemTTL)
 }
 
 // Get gets given key value and expiration time from cache.
-func (c *Cache[K, V]) Get(key K) (V, time.Time) {
+func (c *Cache[K, V]) Get(key K) (*V, *time.Time) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	item := c.cache.Get(key)
 
-	return item.Value(), item.ExpiresAt()
+	if item == nil {
+		return nil, nil
+	}
+
+	v := item.Value()
+	exp := item.ExpiresAt()
+
+	return &v, &exp
 }
 
 // New returns a pointer to a Cache struct instance.
 func New[K comparable, V any]() *Cache[K, V] {
 	// Initializes cache with `defaultTTL`.
 	cache := ttlcache.New[K, V](
-		ttlcache.WithTTL[K, V](defaultTTL),
+		ttlcache.WithTTL[K, V](time.Duration(DefaultTTL)),
 	)
 	go cache.Start()
 
