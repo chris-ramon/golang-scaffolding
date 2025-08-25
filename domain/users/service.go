b452ae9ee31053678dc62b1e40e48b05f3b11eb6
Chris Ramón <rchristian.ramon@gmail.com>
2025-08-25 18:36:07 -0500

    domain/users/service: wires cache pkg

--- domain/users/service.go
+++ domain/users/service.go
@@ -2,20 +2,52 @@ package users
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
 	userTypes "github.com/chris-ramon/golang-scaffolding/domain/users/types"
+	cachePkg "github.com/chris-ramon/golang-scaffolding/pkg/cache"
 )
 
+const findUsersKey string = "findUsersKey"
+
 type service struct {
-	repo Repo
+	repo  Repo
+	cache *cachePkg.Cache[string, string]
 }
 
 func (s *service) FindUsers(ctx context.Context) ([]*userTypes.User, error) {
-	return s.repo.FindUsers(ctx)
+	items, _ := s.cache.Get(findUsersKey)
+	if items != nil {
+		var result []*userTypes.User
+		if err := json.Unmarshal([]byte(*items), &result); err != nil {
+			return nil, err
+		}
+
+		return result, nil
+	}
+
+	users, err := s.repo.FindUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if users == nil {
+		return nil, nil
+	}
+
+	usersJSON, err := json.Marshal(users)
+	if err != nil {
+		return nil, err
+	}
+
+	ttl := cachePkg.TTL(10 * time.Minute)
+	s.cache.Set(findUsersKey, string(usersJSON), &ttl)
+
+	return users, nil
 }
 
-func NewService(repo Repo) *service {
-	return &service{repo: repo}
+func NewService(repo Repo, cache *cachePkg.Cache[string, string]) *service {
+	return &service{repo: repo, cache: cache}
 }
 
 type Repo interface {
